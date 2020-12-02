fd66de4df1d08c60a07d8b1181d1215187e8cf49
danai <danaiefst@gmail.com>
2020-12-02 13:26:55 +0200

    fixed loss

--- test.py
+++ test.py
@@ -3,8 +3,44 @@ import numpy as np
 from matplotlib import pyplot as plt
 import data_handler
 from scipy.ndimage import rotate
+import tracking_nn
+
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+
 
 img = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/data/2195.pt")
 label = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/labels/2195.pt")
 
-data_handler.print_data(img, label)
+label1 = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/labels/2800.pt")
+
+yh = torch.zeros((6, 8, 8), dtype=torch.double)
+x1, y1, x2, y2 = int(label1[0][0]), int(label1[0][1]), int(label1[1][0]), int(label1[1][1])
+yh[0, x1, y1] = 1
+yh[3, x2, y2] = 1
+yh[1, x1, y1] = label1[0][2]
+yh[2, x1, y1] = label1[0][3]
+yh[1, x2, y2] = label1[1][2]
+yh[2, x2, y2] = label1[1][3]
+
+def loss(y_h, y):
+    y = y.to(torch.double)
+    y_h = y_h.to(torch.double)
+    """y are the data labels (format: [[[x_cell1, y_cell1, x_center1, y_center1], [x_cell2, y_cell2, x_center2, y_center2]], ...]), y_h (y hat) is the nn's output"""
+    #print(y_h[:, 0, :, :])
+    p1_h = y_h[:, 0, :, :]
+    p2_h = y_h[:, 3, :, :]
+    p1 = torch.zeros(p1_h.shape)
+    p2 = torch.zeros(p2_h.shape)
+    p1[torch.arange(y.shape[0]), y[:, 0, 0].long(), y[:, 0, 1].long()] = 1
+    p2[torch.arange(y.shape[0]), y[:, 1, 0].long(), y[:, 1, 1].long()] = 1
+    prob_loss = ((p1 - p1_h) ** 2).sum() + ((p2 - p2_h) ** 2).sum()
+    detect_cell1 = p1_h.reshape((p1.size(0), -1)).argmax(axis = 1)
+    detect_cell2 = p2_h.reshape((p2.size(0), -1)).argmax(axis = 1)
+    detect_cell1 = torch.stack((detect_cell1 // 8, detect_cell1 % 8), dim = 1)
+    detect_cell2 = torch.stack((detect_cell2 // 8, detect_cell2 % 8), dim = 1)
+    #print(y, detect_cell1, detect_cell2)
+    detect_loss = ((y_h[torch.arange(p1.size(0)), 1:3, detect_cell1[:, 0], detect_cell1[:, 1]] - y[:, 0, 2:]) ** 2).sum() + ((y_h[torch.arange(p1.size(0)), 4:, detect_cell2[:, 0], detect_cell2[:, 1]] - y[:, 1, 2:]) ** 2).sum() + ((detect_cell1.double() - y[:, 0, :2]) ** 2).sum() + ((detect_cell2.double() - y[:, 1, :2]) ** 2).sum()
+    print(prob_loss / y.shape[0], detect_loss / y.shape[0])
+    return prob_loss + detect_loss
+
+print(loss(torch.stack([yh, yh], dim = 0), torch.stack([label1, label1], dim = 0)))
--- tracking_nn.py
+++ tracking_nn.py
@@ -12,7 +12,7 @@ class Net(Module):
         self.grid = 8
         self.device = device
         self.cnn_layers = Sequential(
-            Conv2d(1, 16, kernel_size=6, stride=2),
+            Conv2d(1, 16, kernel_size=13),
             #BatchNorm2d(16),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
@@ -20,9 +20,16 @@ class Net(Module):
             #BatchNorm2d(32),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
-            Conv2d(32, 32, kernel_size=3),
+            Conv2d(32, 32, kernel_size=5),
+            #BatchNorm2d(32),
+            ReLU(inplace=True),
+            Conv2d(32, 32, kernel_size=5),
+            #BatchNorm2d(32),
+            ReLU(inplace=True),
+            Conv2d(32, 32, kernel_size=5),
             #BatchNorm2d(32),
             ReLU(inplace=True)
+
         )
 
         self.linear_layers = Sequential(
@@ -42,8 +49,8 @@ class Net(Module):
         p2_h = y_h[:, 3, :, :]
         p1 = torch.zeros(p1_h.shape)
         p2 = torch.zeros(p2_h.shape)
-        p1[:, y[:, 0, :2].long()] = 1
-        p2[:, y[:, 1, :2].long()] = 1
+        p1[torch.arange(y.shape[0]), y[:, 0, 0].long(), y[:, 0, 1].long()] = 1
+        p2[torch.arange(y.shape[0]), y[:, 1, 0].long(), y[:, 1, 1].long()] = 1
         prob_loss = ((p1.to(self.device) - p1_h) ** 2).sum() + ((p2.to(self.device) - p2_h) ** 2).sum()
         detect_cell1 = p1_h.reshape((p1.size(0), -1)).argmax(axis = 1)
         detect_cell2 = p2_h.reshape((p2.size(0), -1)).argmax(axis = 1)
@@ -51,6 +58,7 @@ class Net(Module):
         detect_cell2 = torch.stack((detect_cell2 // 8, detect_cell2 % 8), dim = 1).to(self.device)
         #print(y, detect_cell1, detect_cell2)
         detect_loss = ((y_h[torch.arange(p1.size(0)), 1:3, detect_cell1[:, 0], detect_cell1[:, 1]] - y[:, 0, 2:]) ** 2).sum() + ((y_h[torch.arange(p1.size(0)), 4:, detect_cell2[:, 0], detect_cell2[:, 1]] - y[:, 1, 2:]) ** 2).sum() + ((detect_cell1.double() - y[:, 0, :2]) ** 2).sum() + ((detect_cell2.double() - y[:, 1, :2]) ** 2).sum()
+        #print(prob_loss / y.shape[0], detect_loss / y.shape[0])
         return prob_loss + detect_loss
 
     def forward(self, x):
@@ -61,11 +69,11 @@ class Net(Module):
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
         x = x.reshape((x.size(0), 6, self.grid, self.grid))
-        y = x[:, [1, 2, 4, 5], :, :]
-        y = y.view(y.size(0), 1, -1)
-        y, self.h = self.rnn_layers(y, self.h)
-        y = y.reshape((x.size(0), 4, self.grid, self.grid))
-        z = torch.empty(x.shape, dtype=torch.double).to(self.device)
-        z[:, [0, 3], :, :] = x[:, [0, 3], :, :]
-        z[:, [1, 2, 4, 5], :, :] = y
-        return z
+        #y = x[:, [1, 2, 4, 5], :, :]
+        #y = y.view(y.size(0), 1, -1)
+        #y, self.h = self.rnn_layers(y, self.h)
+        #y = y.reshape((x.size(0), 4, self.grid, self.grid))
+        #z = torch.empty(x.shape, dtype=torch.double).to(self.device)
+        #z[:, [0, 3], :, :] = x[:, [0, 3], :, :]
+        #z[:, [1, 2, 4, 5], :, :] = y
+        return x
