f1306a708f7c9b393ce3957cb8c69abe626dd8e9
danai <danaiefst@gmail.com>
2020-12-03 14:07:29 +0200

    made test script

--- test.py
+++ test.py
@@ -5,13 +5,42 @@ import data_handler
 from scipy.ndimage import rotate
 import tracking_nn
 
+def check_out(batch, out):
+    for i in range(out.shape[0]):
+        y = batch[i]
+        yh = out[i]
+        p1_h = yh[0, :, :]
+        p2_h = yh[3, :, :]
+        detect_cell1 = p1_h.reshape(-1).argmax(axis = 0)
+        detect_cell2 = p2_h.reshape(-1).argmax(axis = 0)
+        x_cell1, y_cell1 = detect_cell1 // 8, detect_cell1 % 8
+        x_cell2, y_cell2 = detect_cell2 // 8, detect_cell2 % 8
+        data_handler.print_data(y, [[x_cell1, y_cell1, out[i][1, x_cell1, y_cell1], out[i][2, x_cell1, y_cell1]], [x_cell2, y_cell2, out[i][4, x_cell2, y_cell2], out[i][5, x_cell2, y_cell2]]])
+
+data_paths=["/home/danai/Desktop/GaitTracking/p1/2.a"]#,"/home/danai/Desktop/GaitTracking/p5/2.a", "/home/danai/Desktop/GaitTracking/p11/2.a", "/home/danai/Desktop/GaitTracking/p11/3.a", "/home/danai/Desktop/GaitTracking/p16/3.a", "/home/danai/Desktop/GaitTracking/p17/3.a", "/home/danai/Desktop/GaitTracking/p18/2.a", "/home/danai/Desktop/GaitTracking/p18/3.a"]
+data = data_handler.LegDataLoader(data_paths)
+print("Loading dataset...")
+_, _, val_set_x, _, _, _ = data.load(32)
+
 device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+net = tracking_nn.Net(device)
+net.load_state_dict(torch.load("/home/danai/Desktop/GaitTracking/model.pt", map_location=device))
+for batch in val_set_x:
+    out = net(batch)
+    check_out(batch, out)
 
 
-img = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/data/2195.pt")
-label = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/labels/2195.pt")
 
-label1 = torch.load("/home/danai/Desktop/GaitTracking/p5/2.a/labels/2800.pt")
+
+
+
+
+
+
+
+
+
+
 
 yh = torch.zeros((6, 8, 8), dtype=torch.double)
 x1, y1, x2, y2 = int(label1[0][0]), int(label1[0][1]), int(label1[1][0]), int(label1[1][1])
@@ -39,8 +68,6 @@ def loss(y_h, y):
     detect_cell1 = torch.stack((detect_cell1 // 8, detect_cell1 % 8), dim = 1)
     detect_cell2 = torch.stack((detect_cell2 // 8, detect_cell2 % 8), dim = 1)
     #print(y, detect_cell1, detect_cell2)
-    detect_loss = ((y_h[torch.arange(p1.size(0)), 1:3, detect_cell1[:, 0], detect_cell1[:, 1]] - y[:, 0, 2:]) ** 2).sum() + ((y_h[torch.arange(p1.size(0)), 4:, detect_cell2[:, 0], detect_cell2[:, 1]] - y[:, 1, 2:]) ** 2).sum() + ((detect_cell1.double() - y[:, 0, :2]) ** 2).sum() + ((detect_cell2.double() - y[:, 1, :2]) ** 2).sum()
+    detect_loss = 64 * ((y_h[torch.arange(p1.size(0)), 1:3, detect_cell1[:, 0], detect_cell1[:, 1]] - y[:, 0, 2:]) ** 2).sum() + 64 * ((y_h[torch.arange(p1.size(0)), 4:, detect_cell2[:, 0], detect_cell2[:, 1]] - y[:, 1, 2:]) ** 2).sum() + ((detect_cell1.double() - y[:, 0, :2]) ** 2).sum() + ((detect_cell2.double() - y[:, 1, :2]) ** 2).sum()
     print(prob_loss / y.shape[0], detect_loss / y.shape[0])
     return prob_loss + detect_loss
-
-print(loss(torch.stack([yh, yh], dim = 0), torch.stack([label1, label1], dim = 0)))
