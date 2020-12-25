2f626bad6223beb3e8c3b60003e314b76d845522
danai <danaiefst@gmail.com>
2020-12-25 18:07:50 +0200

    different loss

--- tracking_nn.py
+++ tracking_nn.py
@@ -50,31 +50,21 @@ class Net(Module):
 
         self.rnn_layers = LSTM(input_size = 6 * self.grid * self.grid, hidden_size = 6 * self.grid * self.grid, num_layers = self.num_of_layers, batch_first = True)
 
-    def loss(self, y_h, y):
-        y = y.to(torch.double)
-        y_h = y_h.to(torch.double)
-        """y are the data labels (format: [[[x_cell1, y_cell1, x_center1, y_center1], [x_cell2, y_cell2, x_center2, y_center2]], ...]), y_h (y hat) is the nn's output"""
-        #print(y_h[:, 0, :, :])
-        p1_h = y_h[:, 0, :, :]
-        p2_h = y_h[:, 3, :, :]
-        p1 = torch.zeros(p1_h.shape)
-        p2 = torch.zeros(p2_h.shape)
-        p1[torch.arange(y.shape[0]), y[:, 0, 0].long(), y[:, 0, 1].long()] = 1
-        p2[torch.arange(y.shape[0]), y[:, 1, 0].long(), y[:, 1, 1].long()] = 1
-        prob_loss = abs(p1.to(self.device) - p1_h).sum() + abs(p2.to(self.device) - p2_h).sum()
-        detect_cell1 = p1_h.reshape((p1.size(0), -1)).argmax(axis = 1)
-        detect_cell2 = p2_h.reshape((p2.size(0), -1)).argmax(axis = 1)
-        detect_cell1 = torch.stack((detect_cell1 // self.grid, detect_cell1 % self.grid), dim = 1).to(self.device)
-        detect_cell2 = torch.stack((detect_cell2 // self.grid, detect_cell2 % self.grid), dim = 1).to(self.device)
-        #print(y, detect_cell1, detect_cell2)
-        pos1h = detect_cell1.double() + y_h[torch.arange(p1.size(0)), 1:3, detect_cell1[:, 0], detect_cell1[:, 1]]
-        pos1 = y[:, 0, :2] + y[:, 0, 2:]
-        pos2h = detect_cell2.double() + y_h[torch.arange(p2.size(0)), 4:, detect_cell2[:, 0], detect_cell2[:, 1]]
-        pos2 = y[:, 1, :2] + y[:, 1, 2:]
-        #print(((pos1 - pos1h) ** 2).sum(), ((pos2 - pos2h) ** 2).sum())
-        detect_loss = (torch.sqrt(((pos1 - pos1h) ** 2).sum(axis=1)) + torch.sqrt(((pos2 - pos2h) ** 2).sum(axis=1))).sum()
-        return prob_loss + detect_loss
+    def loss(self, yh, y):
+        #Probability loss
+        prob = yh[:, [0, 3], :, :]
+        probh = torch.zeros(y_h.shape[0], 2, self.grid, self.grid)
+        probh[torch.arange(y.shape[0]), 0, y[:, 0, 0], y[:, 0, 1]] = 1
+        probh[torch.arange(y.shape[0]), 1, y[:, 1, 0], y[:, 1, 1]] = 1
+        prob_loss = torch.abs(prob - probh).sum()
 
+        #Detection loss
+        rlegh = yh[torch.arange(yh.shape[0]), 1:3, y[:, 0, 0], y[:, 0, 1]]
+        llegh = yh[torch.arange(yh.shape[0]), 4:, y[:, 1, 0], y[:, 1, 1]]
+
+        detect_loss = torch.sqrt(((rlegh - y[:, 0, 2:]) ** 2).sum(axis=1)).sum()
+
+        return prob_loss + detect_loss 
 
     def forward(self, x):
         x = x.to(torch.double)
