3eaf825cde917f3f39bcb37e1540381a3edecb54
danai <danaiefst@gmail.com>
2020-12-27 13:58:28 +0200

    only classification

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ data_paths=["/home/danai/Desktop/GaitTracking/p1/2.a","/home/danai/Desktop/GaitT
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Working on", device)
 model = classification.Net(device).to(device)
-data = data_handler.LegDataLoader(data_paths = data_paths, cnn = 1)
+data = data_handler.LegDataLoader(cnn = 1)
 print("Loading dataset...")
 train_set_x, train_set_y, val_set_x, val_set_y, test_set_x, test_set_y = data.load(32)
 
