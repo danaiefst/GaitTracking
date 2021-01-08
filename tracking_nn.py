5bfa5bb4644b45e195f4c145fb9448dd8a16ba42
danai <danaiefst@gmail.com>
2021-01-08 12:47:08 +0200

    added TCN to cnn

--- tracking_nn.py
+++ tracking_nn.py
@@ -1,7 +1,68 @@
 import torch
-from torch.nn import LSTM, Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.nn import Conv1d, LSTM, Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.nn.utils import weight_norm
 
 torch.set_default_dtype(torch.double)
+
+class Chomp1d(Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+
+class TemporalBlock(Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = ReLU()
+        self.dropout1 = Dropout(dropout)
+
+        self.conv2 = weight_norm(Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = ReLU()
+        self.dropout2 = Dropout(dropout)
+
+        self.net = Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+
+class TemporalConvNet(Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
 
 class CNN(Module):
     def __init__(self, device):
@@ -45,9 +106,11 @@ class CNN(Module):
             Linear(784, 512),
             Dropout(0.5),
             ReLU(inplace=True),
-            Linear(512, 294),    #384 = 6*7*7
+            Linear(512, 6 * self.grid * self.grid),    #294 = 6*7*7
             ReLU(inplace=True)
         )
+
+        self.TCN = TemporalConvNet(6 * self.grid * self.grid, [6 * self.grid * self.grid])
 
     def loss(self, yh, y):
         #Probability loss
@@ -67,10 +130,12 @@ class CNN(Module):
 
     def forward(self, x):
         x = x.to(torch.double)
-        x = x.reshape(x.size(0), 1, x.size(1), x.size(2))
+        x = x.view(x.size(0), 1, x.size(1), x.size(2))
         x = self.cnn_layers(x)
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
+        x = x.view(x.size(0), -1, 1)
+        x = self.TCN(x)
         x = x.view(x.size(0), 6, self.grid, self.grid)
         return x
 
