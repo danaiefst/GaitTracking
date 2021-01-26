93b825b8d74ab6a1f5ba0f7ad09d61bc0e859115
danaiefst <danaiefst@gmail.com>
2021-01-27 00:00:42 +0200

    best model, mean 5.9, max 34

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def eucl_dist(out, labels):
 print("Started training...")
 for epoch in range(epochs):
     running_loss = 0
-    if epoch == 10:
+    if epoch == 10 or epoch == 20:
         learning_rate *= 0.1
         optimizer = Adam(model.parameters(), lr = learning_rate)
     for i in range(len(train_set_x)):
