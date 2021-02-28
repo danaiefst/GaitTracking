7845f84c3622b35259f0f491dd1b15295bba48d7
danaiefst <danaiefst@gmail.com>
2021-02-28 11:35:38 +0200

    p18/2.a p1/2.a, CNN 3.55, 31.11, rnn 3.08, 19.56

--- train.py
+++ train.py
@@ -8,13 +8,14 @@ import sys
 from torch.optim import Adam
 
 flag = int(sys.argv[1])
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 print("Working on", device)
-#path = "/home/athdom/GaitTracking/"
-path = "/home/iral-lab/GaitTracking/"
+path = "/home/athdom/GaitTracking/"
+#path = "/home/iral-lab/GaitTracking/"
 #path = "/home/danai/Desktop/GaitTracking/"
 data_path = path + "data/" 
 batch_size = 32
+paths=["p18/3.a", "p5/2.a", "p11/2.a", "p11/3.a", "p16/3.a", "p17/2.a", "p17/3.a", "p18/2.a", "p1/2.a"]
 
 cnn = tracking_nn.CNN().to(device)
 if flag:
@@ -23,7 +24,7 @@ if flag:
         param.requires_grad = False
 rnn = tracking_nn.RNN().to(device)
 model = tracking_nn.Net(device, cnn, rnn).to(device)
-data = data_handler.LegDataLoader(batch_size = batch_size, data_path = data_path)
+data = data_handler.LegDataLoader(batch_size = batch_size, data_path = data_path, paths=paths)
 # Train the nn
 
 epochs = 1000
@@ -60,7 +61,7 @@ def eucl_dist(out, labels):
 print("Started training...")
 for epoch in range(epochs):
     running_loss = 0
-    if epoch == 11:# or epoch == 28:
+    if epoch == 10 or epoch == 25:
         learning_rate *= 0.1
         optimizer = Adam(model.parameters(), lr = learning_rate)
     f, input, label = data.load(0)
