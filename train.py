682c4dee745020a7c352a43da6a4acb2efcbc5b0
gchal <chalvatzaki@ias.informatik.tu-darmstadt.de>
2021-03-04 18:51:41 +0100

    p11 2.a

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ flag = int(sys.argv[1])
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Working on", device) 
 batch_size = 32
-paths = ["p17/3.a", "p18/3.a", "p11/3.a", "p16/3.a", "p17/2.a", "p11/2.a", "p18/2.a", "p1/2.a"]
+paths = ["p17/3.a", "p18/3.a", "p11/3.a", "p16/3.a", "p17/2.a", "p1/2.a", "p18/2.a", "p11/2.a"]
 
 cnn = tracking_nn.CNN().to(device)
 if flag:
@@ -57,7 +57,7 @@ def eucl_dist(out, labels):
 print("Started training...")
 for epoch in range(epochs):
     running_loss = 0
-    if epoch == 10 or epoch == 25:
+    if epoch == 25 or epoch == 50:
         learning_rate *= 0.1
         optimizer = Adam(model.parameters(), lr = learning_rate)
     f, input, label = data.load(0)
